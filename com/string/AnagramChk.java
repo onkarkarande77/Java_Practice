5f091536bd4c0e29a8e3cbb3072f12ca0350efe7
onkarkarande77 <karandeonkar05@gmail.com>
2025-11-27 13:43:04 +0530

    string practice

---
+++ com/string/AnagramChk.java
@@ -0,0 +1,26 @@
+package com.string;
+
+import java.util.Arrays;
+
+public class AnagramChk {
+
+	public static void main(String[] args) {
+		//simple string
+		
+		String str1="silent";
+		String str2="listen";
+		
+	   char []a1=str1.toCharArray();
+	   char []a2=str2.toCharArray();
+		
+	   Arrays.sort(a1);
+	   Arrays.sort(a2);
+	   
+       if(Arrays.equals(a1,a2)) {
+    	   System.out.println("this is an anagram");
+       }	   
+       else {
+    	   System.out.println("this is not anagram");
+       }
+	}
+}
