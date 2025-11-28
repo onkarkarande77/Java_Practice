b6041ab60a529c12e1d96bd90bce55ac85082b5e
onkarkarande77 <karandeonkar05@gmail.com>
2025-11-28 12:37:39 +0530

    string practice

--- com/string/AnagramChk.java
+++ com/string/AnagramChk.java
@@ -1,15 +1,11 @@
 package com.string;
 
-<<<<<<< HEAD
-=======
- 
->>>>>>> fe3a080a89bde9ccd1d321ccb11dcdc9c0b0e6bf
 import java.util.Arrays;
 
 public class AnagramChk {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+ 
 		//simple string
 		
 		String str1="silent";
@@ -28,25 +24,10 @@ public class AnagramChk {
     	   System.out.println("this is not anagram");
        }
 	}
-=======
+ 
 		// TODO Auto-generated method stub
-		String str1="listen";
-		String str2="silent";
-		
-		char[] ar1=str1.toCharArray();
-		char[] ar2=str2.toCharArray();
-		
-		Arrays.sort(ar1);
-		Arrays.sort(ar2);
-		
-		if(Arrays.equals(ar1,ar2)) {
-			System.out.println("this is an anagram");
-		}
-		else {
-			System.out.println("it was not anagram");
-		}
-         
+		 
 	}
 
->>>>>>> fe3a080a89bde9ccd1d321ccb11dcdc9c0b0e6bf
-}
+ 
+
