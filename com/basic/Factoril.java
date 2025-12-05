4d81f0a0877788f42a1ecf03533e17197b74f126
onkarkarande77 <karandeonkar05@gmail.com>
2025-12-05 13:05:45 +0530

    this is basic

---
+++ com/basic/Factoril.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class Factoril {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=5,fact=1;
+		
+		for(int i=1;i<=num;i++) {
+			fact=fact*i;
+		}
+		System.out.println("factorial os the given no is "+fact);
+	}
+
+}
---
+++ com/basic/UsingScannerFindFactorial.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class UsingScannerFindFactorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Number ");
+		int num=sc.nextInt();
+		int fact=1;
+		
+		for(int i=1;i<=num;i++) {
+			fact*=i;
+		}
+	 	
+   System.out.println("the factorial "+fact);
+	}
+
+}
