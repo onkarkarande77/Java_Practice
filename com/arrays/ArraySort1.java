4365b94759cde989865015190c389c078247909d
onkarkarande77 <karandeonkar05@gmail.com>
2025-12-07 15:51:57 +0530

    array sorting

---
+++ com/arrays/ArraySort1.java
@@ -0,0 +1,26 @@
+package com.arrays;
+
+public class ArraySort1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ar[]= {3,2,7,9,1};
+		
+		
+		for(int i=0;i<ar.length;i++) {
+			
+		  for(int j=0;j<ar.length;j++) {
+			  if(ar[i]<ar[j]) {
+				  int temp=ar[i];
+				      ar[i]=ar[j];
+				      ar[j]=temp;
+				  }
+		  }
+		}
+		
+		for(int num:ar) {
+			System.out.println(num);
+		}
+	}
+
+}
---
+++ com/arrays/SortArray.java
@@ -0,0 +1,16 @@
+package com.arrays;
+
+import java.util.Arrays;
+
+public class SortArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int ar[]= {3,2,7,9,1};
+       
+       Arrays.sort(ar);
+       
+       System.out.println(Arrays.toString(ar));
+	}
+
+}
