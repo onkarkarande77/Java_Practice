1032348cea94db38e523564cf6f00a422ea5a6c3
onkarkarande77 <karandeonkar05@gmail.com>
2025-12-09 13:21:48 +0530

    factorial of no

---
+++ com/AllBasic/FactorialNo.java
@@ -0,0 +1,22 @@
+package com.AllBasic;
+
+import java.util.Scanner;
+
+public class FactorialNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         Scanner sc=new Scanner(System.in);
+         System.out.println("Enter the No ");
+         int num=sc.nextInt();
+         
+         int fact=1;
+         
+         for(int i=1;i<=num;i++) {
+        	 fact*=i;
+         }
+         
+         System.out.println("factorial of the no is "+fact);
+	}
+
+}
