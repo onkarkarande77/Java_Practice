75bbdb9a1881786aa572856ac5fd7e1177a493ec
onkarkarande77 <karandeonkar05@gmail.com>
2026-03-23 22:51:05 +0530

    this is the code of Binary Triangle Pattern

---
+++ com/arrays/BinaryTrianglePattern.java
@@ -0,0 +1,34 @@
+package com.arrays;
+
+public class BinaryTrianglePattern {
+
+    public static void main(String[] args) {
+
+        int n = 6;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                // Condition to alternate 1 and 0
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+
+// OUT PUT LOOK LIKE
+
+/*1 
+  0 1 
+  1 0 1 
+  0 1 0 1 
+  1 0 1 0 1 
+  0 1 0 1 0 1 */
