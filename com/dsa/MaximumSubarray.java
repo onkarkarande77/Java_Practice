055d1e9dd01abaee87c4915e847c26751ef445f8
onkarkarande77 <karandeonkar05@gmail.com>
2026-04-20 22:31:06 +0530

    this code for the finding  Maximum Sub array

---
+++ com/dsa/MaximumSubarray.java
@@ -0,0 +1,28 @@
+package com.dsa;
+
+public class MaximumSubarray {
+    public static int maxSubArray(int[] nums) {
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            currentSum += num;
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+
+            // reset if negative
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums)); // Output: 6
+    }
+}
